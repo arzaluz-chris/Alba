3be28f2e8c33563ccde92f1be3c9807e177a9a16
arzaluz-chris <23008381@es.uveg.edu.mx>
2026-03-24 20:18:10 -0600

    Initial Commit

---
+++ Alba/AlbaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbaApp.swift
+//  Alba
+//
+//  Created by Christian Arzaluz on 24/03/26.
+//
+
+import SwiftUI
+
+@main
+struct AlbaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
